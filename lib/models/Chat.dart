173453355070846e81434f48645fefaa6ba8baa3
Mastermindddddd <124505009+Mastermindddddd@users.noreply.github.com>
2023-05-08 12:35:05 +0200

    Update Chat.dart
    defined iconNumber, unreadCount and password

--- lib/models/Chat.dart
+++ lib/models/Chat.dart
@@ -1,73 +1,100 @@
 class Chat {
-  final String groupName, lastMessage, icon, time;
+  final String groupName;
+  final String lastMessage;
+  final int iconNumber;
+  final String time;
+  final String password;
+  final int unreadCount;
 
   Chat({
     required this.groupName,
     required this.lastMessage,
-    required this.icon,
+    required this.iconNumber,
     required this.time,
+    required this.password,
+    this.unreadCount = 0,
   });
 }
 
 List<Chat> chatsData = [
   Chat(
     groupName: "Group 1",
-    lastMessage: "Hey guys, what's up?",
-    icon: "1",
-    time: "10:30 AM",
+    lastMessage: "Welcome to Group 1",
+    iconNumber: 1,
+    time: "3m ago",
+    password: "group1",
+    unreadCount: 2,
   ),
   Chat(
     groupName: "Group 2",
-    lastMessage: "Anyone have the password?",
-    icon: "2",
-    time: "11:45 AM",
+    lastMessage: "Welcome to Group 2",
+    iconNumber: 2,
+    time: "8m ago",
+    password: "group2",
+    unreadCount: 1,
   ),
   Chat(
     groupName: "Group 3",
-    lastMessage: "Let's plan for the weekend trip!",
-    icon: "3",
-    time: "Yesterday",
+    lastMessage: "Welcome to Group 3",
+    iconNumber: 3,
+    time: "5d ago",
+    password: "group3",
+    unreadCount: 0,
   ),
   Chat(
     groupName: "Group 4",
-    lastMessage: "Who's bringing the snacks?",
-    icon: "4",
-    time: "2d ago",
+    lastMessage: "Welcome to Group 4",
+    iconNumber: 4,
+    time: "5d ago",
+    password: "group4",
+    unreadCount: 0,
   ),
   Chat(
     groupName: "Group 5",
-    lastMessage: "Don't forget to submit the project by Monday",
-    icon: "5",
-    time: "2d ago",
+    lastMessage: "Welcome to Group 5",
+    iconNumber: 5,
+    time: "6d ago",
+    password: "group5",
+    unreadCount: 0,
   ),
   Chat(
     groupName: "Group 6",
-    lastMessage: "Did anyone read chapter 5 for the exam?",
-    icon: "6",
-    time: "3d ago",
+    lastMessage: "Welcome to Group 6",
+    iconNumber: 6,
+    time: "3m ago",
+    password: "group6",
+    unreadCount: 1,
   ),
   Chat(
     groupName: "Group 7",
-    lastMessage: "Let's meet at the coffee shop at 5",
-    icon: "7",
-    time: "3d ago",
+    lastMessage: "Welcome to Group 7",
+    iconNumber: 7,
+    time: "8m ago",
+    password: "group7",
+    unreadCount: 0,
   ),
   Chat(
     groupName: "Group 8",
-    lastMessage: "I'm running late, save me a seat!",
-    icon: "8",
-    time: "4d ago",
+    lastMessage: "Welcome to Group 8",
+    iconNumber: 8,
+    time: "5d ago",
+    password: "group8",
+    unreadCount: 2,
   ),
   Chat(
     groupName: "Group 9",
-    lastMessage: "We need to reschedule the meeting",
-    icon: "9",
-    time: "4d ago",
+    lastMessage: "Welcome to Group 9",
+    iconNumber: 9,
+    time: "5d ago",
+    password: "group9",
+    unreadCount: 0,
   ),
   Chat(
     groupName: "Group 10",
-    lastMessage: "Who's up for a game night this weekend?",
-    icon: "10",
-    time: "5d ago",
+    lastMessage: "Welcome to Group 10",
+    iconNumber: 10,
+    time: "6d ago",
+    password: "group10",
+    unreadCount: 0,
   ),
 ];
